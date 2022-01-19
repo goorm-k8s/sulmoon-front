30465e6746ebbbc37415f8b1327059f3ffb89c3a
hxunz <guswl2843@gmail.com>
2022-01-19 20:23:12 +0900

    ✨ 로그인 화면 id, password 구성

---
+++ src/Input.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Input({ value, onChange }) {
+  return (
+    <>
+    <p>
+      <label htmlFor="input-task-title">
+        LOG IN
+      </label>
+    </p>
+    <p>
+      <input
+        id="input-task-title"
+        type="text"
+        placeholder="ID"
+        value={value}
+        onChange={onChange}
+      />
+    </p>
+    <p>
+      <input
+        id="input-task-title"
+        type="text"
+        placeholder="password"
+        value={value}
+        onChange={onChange}
+      />
+      </p>
+      </>
+  );
+}
