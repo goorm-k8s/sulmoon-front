fa54bad83fb5d74eaf165b4c9eb68e64ba7f729f
hxunz <guswl2843@gmail.com>
2022-01-28 14:39:29 +0900

    ✏️ 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
-import CreateSulmoon from './pages/CreateSulmoon';
+import CreateSurvey from './pages/CreateSurvey';
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/Login" exact element={<Login />} />
         <Route path="/oauth/kakao/callback" element={<Auth />} />
-        <Route path="/CreateSulmoon" element={<CreateSulmoon />} />
+        <Route path="/CreateSurvey" element={<CreateSurvey />} />
       </Routes>
     </Router>
   );
