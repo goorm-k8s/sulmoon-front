b7084c16e08622bcf684de5999019f390615709d
hxunz <guswl2843@gmail.com>
2022-02-04 02:49:24 +0900

    kakao access token 발급

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const formUrlEncoded = x =>
+    Object.keys(x).reduce((p, c) => p + `&${c}=${encodeURIComponent(x[c])}`, '')
 
 const Auth = () => {
-  const code = new URL(window.location.href).searchParams.get('code');
+  const code = new URL(window.location.href).searchParams.get('code');  // 카카오에서 인가받은 코드
+
+  // 비동기호출 -> 함수를 먼가 실행해야하는건데 -> 리액트에서는 이걸 이팩트를 발생시켜서 처리할수있다
+  useEffect(() => {
+    if (code) {
+      kakaoLogin(code);
+    }
+  }, [code]);
+  
+
+  const kakaoLogin = async (code) => {
+    const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
+
+    const user = {
+      'grant_type': 'authorization_code',
+      'client_id': '5af42f065a8d4e342c4249779e84b76e',
+      'redirect_uri': REDIRECT_URI,
+      'code': code
+      // client_secret: 'YrxkYSpDQFEPnS4iKcPlOYmu7Yp0NCII',
+    }
+
+    const response = await fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded'
+      },
+      body: formUrlEncoded(user)
+    })
+    console.log(await response.json()) // kakao response
+
+    if (response.access_token) {
+      sulmoonLogin(response.access_token);
+    }
+  }
+  
+  //TODO: kakao response에서 받아온 accesstoken으로 설문 로그인 API를 호출하고 응답 받아서 처리하면 끝!
+  const sulmoonLogin = (token) => {
+    // TODO: async fetch
+  }
 
   return (
     <div>
