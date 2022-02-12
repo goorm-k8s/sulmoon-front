c7fce9c4e872b62e77fbafaa0c181115e5f3f598
hxunz <guswl2843@gmail.com>
2022-02-12 19:22:55 +0900

    생성된 설문 리스트 확인 페이지 api 연동

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,27 +3,27 @@ import "./Card.css"
 import StorageIcon from '@material-ui/icons/Storage';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 // import doc_image from "./t-shirt.png"
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 
-function Card({name}) {
-    const history = useHistory();
+function Card({ title }) {
+    // const history = useHistory();
 
-    function navigate_to(docname){
-        var fname=docname.split(".");
-        history.push("/form/" + fname[0])
-
+    function handleMoveSurvey() {
+        // TODO: 설문 페이지로 이동하기
+        // var fname=docname.split(".");
+        // history.push("/form/" + fname[0])
     }
+
     return (
-        <div className="doc_card" onClick={()=>{
-            navigate_to(name)
-        }}>
+        <div className="doc_card" onClick={handleMoveSurvey}>
              {/* <img src={doc_image} className="doc_card_image"></img> */}
              <div className="doc_card_content">
-                <h5 style={{overFlow:"ellipsis"}}>{name ? name : " Untitled Doc" }</h5>
+                <h5 style={{overFlow:"ellipsis"}}>{title ? title : " Untitled Doc" }</h5>
                 <div className="doc_content">
                     <div className="content_left" style={{fontSize:"12px",color:"grey"}}>
-                    <StorageIcon style={{color:"white",fontSize:"12px",backgroundColor:"#6E2594",padding:"3px",marginRight:"3px",borderRadius:"2px"}}/>   Opened 6 Jan 2021
+                        <StorageIcon style={{ color: "white", fontSize: "12px", backgroundColor: "#6E2594", padding: "3px", marginRight: "3px", borderRadius: "2px" }} />
+                        Opened 6 Jan 2021
                     </div>
                     
                      <MoreVertIcon style={{color:"grey",fontSize:"16px"}} />
