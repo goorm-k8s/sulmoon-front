291da9f755f2d1e1d085d6c48821238373f6393e
hxunz <guswl2843@gmail.com>
2022-02-15 02:40:33 +0900

    설문 생성자 입장에서 설문 조회 api 연동

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
 import QuestionForm from './components/QuestionForm';
+import CreatedSurvey from './components/CreatedSurvey';
 import CreatedSurveys from './pages/CreatedSurveys';
 import AnsweredSurveys from './pages/AnsweredSurveys';
 import { QuestionsProvider } from './hooks/useQuestions';
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="/oauth/kakao/callback" component={Auth} />
           <Route path="/QuestionForm/:id" exact component={QuestionForm} />
           <Route path="/CreatedSurveys" exact component={CreatedSurveys} />
+          <Route path="/CreatedSurvey/:id" exact component={CreatedSurvey} />
           <Route path="/AnsweredSurveys" exact component={AnsweredSurveys} />
         </Switch>
       </Router>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,34 +2,29 @@ import React from 'react'
 import "./Card.css"
 import StorageIcon from '@material-ui/icons/Storage';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-// import doc_image from "./t-shirt.png"
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 
-function Card({ title }) {
-    // const history = useHistory();
+function Card({ title, id }) {
+  const history = useHistory();
 
-    function handleMoveSurvey() {
-        // TODO: 설문 페이지로 이동하기
-        // var fname=docname.split(".");
-        // history.push("/form/" + fname[0])
+  function handleMoveSurvey() {
+    history.push(`/CreatedSurvey/${id}`);
     }
 
-    return (
-        <div className="doc_card" onClick={handleMoveSurvey}>
-             {/* <img src={doc_image} className="doc_card_image"></img> */}
-             <div className="doc_card_content">
-                <h5 style={{overFlow:"ellipsis"}}>{title ? title : " Untitled Doc" }</h5>
-                <div className="doc_content">
-                    <div className="content_left" style={{fontSize:"12px",color:"grey"}}>
-                        <StorageIcon style={{ color: "white", fontSize: "12px", backgroundColor: "#6E2594", padding: "3px", marginRight: "3px", borderRadius: "2px" }} />
-                        Opened 6 Jan 2021
-                    </div>
-                    
-                     <MoreVertIcon style={{color:"grey",fontSize:"16px"}} />
-                </div>
-             </div>
-        </div>
+  return (
+    <div className="doc_card" onClick={handleMoveSurvey}>
+      <div className="doc_card_content">
+        <h5 style={{overFlow:"ellipsis"}}>{title ? title : " Untitled Doc" }</h5>
+          <div className="doc_content">
+            <div className="content_left" style={{fontSize:"12px",color:"grey"}}>
+              <StorageIcon style={{ color: "white", fontSize: "12px", backgroundColor: "#6E2594", padding: "3px", marginRight: "3px", borderRadius: "2px" }} />
+                        {/* Opened 6 Jan 2021 */}
+            </div>
+              <MoreVertIcon style={{color:"grey",fontSize:"16px"}} />
+          </div>
+      </div>
+    </div>
     )
 }
 
