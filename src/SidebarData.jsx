c5988cebc2b33e8a1b78d953c8bc0985e1442681
hxunz <guswl2843@gmail.com>
2022-05-13 10:42:09 +0900

    ♻️ refactor code

--- src/SidebarData.jsx
+++ src/SidebarData.jsx
@@ -3,12 +3,6 @@ import * as BsIcons from 'react-icons/bs';
 import {BiLogIn} from 'react-icons/bi';
 
 export const SidebarData = [
-  // {
-  //   title: '검사하기',
-  //   path: '/ToExamine',
-  //   icon: <BsIcons.BsPersonBoundingBox />,
-  //   cName: 'nav-text',
-  // },
   {
     title: 'Home',
     path: '/',
@@ -21,16 +15,4 @@ export const SidebarData = [
     icon: <BiLogIn />,
     cName: 'nav-text',
   },
-  // {
-  //   title: 'About',
-  //   path: '/About',
-  //   icon: <BsIcons.BsFillInfoCircleFill />,
-  //   cName: 'nav-text',
-  // },
-  // {
-  //   title: 'Contact Us',
-  //   path: '/Contact',
-  //   icon: <BsIcons.BsEnvelopeFill />,
-  //   cName: 'nav-text',
-  // },
 ];
